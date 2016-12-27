bcd412550cda6ec89c631c8d3292afdb1cfbb122
Benno van den Berg <hatchan@gmail.com>
2016-12-27 15:11:19 +0100

    Add conf package

---
+++ conf/util.go
@@ -0,0 +1,15 @@
+package conf
+
+import "gopkg.in/urfave/cli.v1"
+
+// ConcatFlags concatenates all flags provided to a single array. Currently it
+// does not inspect if an flag already exists, or sort by name etc.
+func ConcatFlags(flags ...[]cli.Flag) []cli.Flag {
+	result := []cli.Flag{}
+
+	for _, f := range flags {
+		result = append(result, f...)
+	}
+
+	return result
+}
