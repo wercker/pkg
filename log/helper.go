8cdaec27ff2c925567ddb5c60ef92729990a5647
Guillaume Fillon <kokaz@users.noreply.github.com>
2017-06-30 13:50:12 +0200

    Set logger formatter to JSON when output is not a terminal (#33)

--- log/helper.go
+++ log/helper.go
@@ -1,12 +1,21 @@
 package log
 
 import (
+	"os"
+
+	"github.com/Sirupsen/logrus"
 	cli "gopkg.in/urfave/cli.v1"
 )
 
 func SetupLogging(c *cli.Context) error {
 	if c.GlobalBool("debug") {
 		SetLevel(DebugLevel)
+	}
+
+	// Dynamically return false or true based on the logger output's
+	// file descriptor referring to a terminal or not.
+	if !(os.Getenv("TERM") == "dumb" || !logrus.IsTerminal(logrus.StandardLogger().Out)) {
+		SetFormatter(&logrus.JSONFormatter{})
 	}
 	return nil
 }
